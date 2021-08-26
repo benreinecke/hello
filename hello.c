599a2dd09f8fa8f030443b387d18f8879fd2a6b4
benreinecke <breinecke2@huskers.unl.edu>
2021-08-26 19:43:17 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,16 +1,17 @@
-  /*
-  *
-  *Author: Ben Reinecke
-  *
-  *Date: 2021/08/26
-  *
-  */
-  #include <stdlib.h>
-  #include <stdio.h>
+/**
+ * Author: Ben Reinecke
+ * 
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
-    
-    printf("Ben Reinecke\n");
-    
-    return 0;      
+
+  printf("Ben Reinecke\n");
+
+  return 0;
 }
