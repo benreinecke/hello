940d88dff8cc979657d4373e46f3c202e8a47421
benreinecke <breinecke2@huskers.unl.edu>
2021-08-26 19:16:05 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+  /*
+  *
+  *Author: Ben Reinecke
+  *
+  *Date: 2021/08/26
+  *
+  */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Ben Reinecke\n");
+    
+    return 0;      
+}
